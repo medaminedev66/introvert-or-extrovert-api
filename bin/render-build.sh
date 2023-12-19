868b7e7311a03caa293a776adbfc6c7849b6fc33
Amine Smahi <md.amine.smahi@gmail.com>
2023-12-19 16:15:08 +0100

    Remove two commands to render-build.sh

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,7 +3,7 @@
 set -o errexit
 
 bundle install
-./bin/rails assets:precompile
-./bin/rails rake assets:clean
+# ./bin/rails assets:precompile
+# ./bin/rails rake assets:clean
 ./bin/rails rake db:migrate
 ./bin/rails rake db:seed
